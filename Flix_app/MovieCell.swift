9b90ee723aeb2d1b0e84797e3c5ceddcc68a7756
Collin Scott <collin.scott@bison.howard.edu>
2018-03-06 03:53:42 -0500

    Project finished

---
+++ Flix_app/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flix_app
+//
+//  Created by Imani P. on 3/5/18.
+//  Copyright © 2018 Collin Scott. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
